b5794d91a94a1d3a99be4bf8aa814b1da58b5fb4
juyeonIee <wnurilhamidah@gmail.com>
2025-03-10 14:43:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ window.addEventListener('load', () => {
 
 const messages = [
     {
-        text: "plss mau dong",
-        image: "https://i.pinimg.com/736x/1e/7c/6f/1e7c6ff7a0c26f7eaefbce683656193d.jpg"
+        text: "Hi, sweetie!",
+        image: "https://pin.it/39m09D2xS.jpg"
     },
     {
-        text: "pls klik yg kiri",
-        image: "https://i.pinimg.com/736x/90/73/a9/9073a9359bff531cd830ae7384752934.jpg"
+        text: "Is today your birthday?",
+        image: "https://pin.it/6pB8socru.jpg"
     },
     {
-        text: "terakhir, kalo \"no\" yauda deh",
-        image: "https://i.pinimg.com/736x/a6/43/f5/a643f5a40db387585b56bc767b66fddb.jpg"
+        text: "Happiest Birthday! Please continue your journey to another place.",
+        image: "https://pin.it/4BMGvS8nC.jpg"
     }
 ];
 
